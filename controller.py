9e568466d53acc2a67664025dc4714bcdff73308
gwint1 <gregwint95@gmail.com>
2018-07-22 21:42:34 -0700

    websockets library used instead of trying to implement websocket protocol by myself

--- controller.py
+++ controller.py
@@ -2,19 +2,32 @@ from subprocess import call, Popen
 
 import socket
 import sys
-from thread import *
+#from thread import *
 from time import strftime, gmtime
 import os.path
 import time
 import hashlib
 import base64
+import asyncio
+import websockets
 
 SERVER_NAME = "Greg's Server"
 
 host = ''
 port = 9999
 
+async def time(websocket, path):
+    while True:
+        now = "test"
+        await websocket.send(now)
+        await asyncio.sleep(3)
+
 def main():
+  start_server = websockets.serve(time, 'localhost', 9999)
+
+  asyncio.get_event_loop().run_until_complete(start_server)
+  asyncio.get_event_loop().run_forever()
+  '''
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
   try:
@@ -28,34 +41,30 @@ def main():
   print("Waiting for connections")
 
   try:
-    while True:
-      conn, addr = s.accept()
-      print(addr)
-      init_request = conn.recv(4096)
-      print(init_request)
+    ##while True:
+      #conn, addr = s.accept()
+      #print(addr)
+      #init_request = conn.recv(4096)
+      #print(init_request)
 
-      request_pieces = init_request.split('\r\n')
-      print(request_pieces[-4])
+      #request_pieces = init_request.split('\r\n')
+      #print(request_pieces[-4])
 
-      request_key = request_pieces[-4].split(':')[1].strip()
-      new_key = request_key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-      hashed_new_key = hashlib.sha1(new_key).digest()
-      print(hashed_new_key)
+      #request_key = request_pieces[-4].split(':')[1].strip()
+      #new_key = request_key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+      #hashed_new_key = hashlib.sha1(new_key).digest()
+      #print(hashed_new_key)
 
-      encoded_hashed_new_key = base64.b64encode(hashed_new_key)
-      print(encoded_hashed_new_key)
+      #encoded_hashed_new_key = base64.b64encode(hashed_new_key)
+      #print(encoded_hashed_new_key)
 
-      response = "HTTP/1.1 101 Switching Protocols\r\n" + \
-                 "Upgrade: websocket\r\n" + \
-                 "Connection: Upgrade\r\n" + \
-                 "Sec-WebSocket-Accept: %s\r\n" % encoded_hashed_new_key + \
-                 "WebSocket-Origin: http://localhost\r\n\n"
+      #response = "HTTP/1.1 101 Switching Protocols\r\n" + \
+      #           "Upgrade: websocket\r\n" + \
+      #           "Connection: Upgrade\r\n" + \
+      #           "Sec-WebSocket-Accept: %s\r\n" % encoded_hashed_new_key + \
+      #           "WebSocket-Origin: http://localhost\r\n\n"
 
-      conn.sendall(response)
-
-      ## Form response and send back to client
-      #resp = conn.recv(4096)
-      #print(resp)
+      #conn.sendall(response)
 
       ## Wait to recieve port list from client
 
@@ -68,6 +77,6 @@ def main():
   except KeyboardInterrupt:
     s.shutdown(socket.SHUT_RDWR)
     s.close()
-
+  '''
 
 main()
