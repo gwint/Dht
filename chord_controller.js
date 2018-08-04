3d5917bb2aec072d79172a8a70f2c2d252528465
gwint1 <gregwint95@gmail.com>
2018-08-04 00:29:12 -0700

    Can add as many port-ip entry fields as needed and working on locking in choices

--- chord_controller.js
+++ chord_controller.js
@@ -27,18 +27,43 @@ function create_dht() {
   }
 }
 
-function add_host_port_tuple() {
-  let host_label = '<label for="host">IP:</label>';
-  let host_entry = '<input id="host" type="text"/>';
-  let port_label = '<label for="port">Port:</label>';
-  let port_entry = '<input type="text"/>';
+class Host_IP_Tuple_Collection {
+  constructor(host, port) {
+    this.host = host;
+    this.port = port;
+    this.host_port_tuples = [];
+  }
 
-  html_str = host_label + host_entry + port_label + port_entry + "<br>";
+  get_host() {
+    return this.host;
+  }
 
-  jQuery("#host_ip_tuple_panel").append(html_str);
+  get_port() {
+    return this.port;
+  }
+
+  add_tuple(host, port) {
+    this.host_port_tuples.push([host, port]);
+  }
 }
 
+function add_host_port_tuple() {
+  let host_label = '<label for="host">IP:</label>';
+  let host_entry = '<input class="host_entry entry" id="host" type="text"/>';
+  let port_label = '<label for="port">Port:</label>';
+  let port_entry = '<input class="port_entry entry" type="text"/>';
+
+  html_str = host_label + host_entry + port_label +
+             port_entry + "<br>";
+
+  jQuery("#host_ip_tuple_entry_area").append(html_str);
+}
+
+//function previous
+
 jQuery(document).ready(function() {
+  let host_ip_tuples = new Host_IP_Tuple_Collection();
+
   jQuery("#create_dht_btn").click(function() {
     jQuery("#btn_response_area").text("DHT CREATED");
     create_dht();
@@ -59,4 +84,28 @@ jQuery(document).ready(function() {
   });
 
   jQuery("#add_host_ip_tuple_btn").click(add_host_port_tuple);
+
+  jQuery("#commit_host_port_tuples_btn").click(function() {
+    let hosts = [];
+    let ports = [];
+    //loop through all hosts and ports
+    jQuery("#host_ip_tuple_entry_area").children().each(function() {
+      let has_classes = jQuery(this).attr("class") != undefined;
+
+      if(has_classes) {
+        let class_list = jQuery(this).attr("class").split(" ");
+        for(let idx in class_list) {
+          class_name = class_list[idx];
+          if(class_name == "host_entry") {
+            hosts.push(jQuery(this).val());
+          }
+          if(class_name == "port_entry") {
+            ports.push(jQuery(this).val());
+          }
+        }
+      }
+    });
+    alert(hosts);
+    alert(ports);
+  });
 });
