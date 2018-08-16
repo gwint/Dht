6bd42027a9a8f668f0d44346b4a6d34a0ed1c1fa
gwint1 <gregwint95@gmail.com>
2018-08-15 19:54:12 -0700

    Controller now running init.py after port list is recieved from web client

--- controller.py
+++ controller.py
@@ -15,6 +15,7 @@ NODES_FILE = "nodes.txt"
 HOST = "localhost"
 PORT = 9999
 
+
 def create_nodes_file(port_list):
   nodes_file_obj = open(NODES_FILE, 'w')
 
@@ -23,19 +24,36 @@ def create_nodes_file(port_list):
 
   nodes_file_obj.close()
 
+def generate_node_file(host_port_pairs):
+  node_file_obj = open(NODES_FILE, 'w')
+  for host, port in host_port_pairs:
+    node_file_obj.write("%s:%d\n" % (host, port))
+  node_file_obj.close()
+
 async def control(websocket, path):
   ## Get port numbers from client
-  port_list_str = await websocket.recv()
+  item_str = await websocket.recv()
   port_list = []
 
-  if port_list_str[0] == '[':
-    port_list = list(map(int, port_list_str.strip('[]').split(',')))
+  if item_str[0] == '[':
+    port_list = list(map(int, item_str.strip('[]').split(',')))
+    for port in port_list:
+      Popen(["python", "server.py"] + [str(port)])
+    ## Generate nodes.txt given list of ports and hosts
+    generate_node_file([("127.0.0.1", port) for port in port_list])
+    ## Then run init.py to populate finger tables
+    Popen(["python", "init.py", NODES_FILE])
   else:
-    print(type(json.loads(port_list_str)))
-    print(port_list_str)
+    print(type(json.loads(item_str)))
+    command_obj = json.loads(item_str)
+    print(command_obj)
+    ## Get everything needed to make RPC to one of the nodes
+    ## according to information in command_obj
+    command_id = command_obj["command_id"]
+    target = command_obj["target"]
+    labels = command_obj["labels"]
+    values = command_obj["values"]
 
-  for port in port_list:
-    Popen(["python", "server.py"] + [str(port)])
 
 
 def main():
