b9a77bf612e0e7811fc2783efb9e64c97dff3281
gwint1 <gregwint95@gmail.com>
2018-08-15 21:04:35 -0700

    Added todo list

--- controller.py
+++ controller.py
@@ -1,5 +1,4 @@
 from subprocess import call, Popen
-
 import socket
 import sys
 from time import strftime, gmtime
@@ -10,6 +9,17 @@ import base64
 import asyncio
 import websockets
 import json
+import glob
+sys.path.append('gen-py')
+sys.path.insert(0, glob.glob('/usr/lib/python2.7/site-packages')[0])
+
+from chord import FileStore
+from chord.ttypes import RFile, RFileMetadata, NodeID, SystemException
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
 
 NODES_FILE = "nodes.txt"
 HOST = "localhost"
@@ -44,17 +54,28 @@ async def control(websocket, path):
     ## Then run init.py to populate finger tables
     Popen(["python", "init.py", NODES_FILE])
   else:
-    print(type(json.loads(item_str)))
     command_obj = json.loads(item_str)
     print(command_obj)
-    ## Get everything needed to make RPC to one of the nodes
-    ## according to information in command_obj
     command_id = command_obj["command_id"]
     target = command_obj["target"]
     labels = command_obj["labels"]
     values = command_obj["values"]
 
+    command_args = dict(zip(labels, values))
 
+    if(command_id == "read_btn"):
+      # Make socket
+      transport = TSocket.TSocket(target["host"], target["port"])
+      # Buffering is critical. Raw sockets are very slow
+      transport = TTransport.TBufferedTransport(transport)
+      # Wrap in a protocol
+      protocol = TBinaryProtocol.TBinaryProtocol(transport)
+      # Create a client to use the protocol encoder
+      client = FileStore.Client(protocol)
+      # Connect!
+      transport.open()
+
+      client.readFile(command_args["File Name:"], command_args["Owner:"])
 
 def main():
   start_server = websockets.serve(control, 'localhost', PORT)
